1053e52b85d201aca24de8dd12a8721c6bd7ad57
bcoe <briancoe@gmail.com>
2019-05-29 12:16:28 -0700

    created a grid with random colors, to be modified

---
+++ ProjectFiles/FlatCell/Assets/Scripts/makePlaneGrid.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class makePlaneGrid : MonoBehaviour
+{
+
+    public int xSize, ySize, gridSize;
+    public Transform gridPlane;
+
+    private void Awake()
+    {
+        Generate();
+    }
+
+    private void Generate()
+    {
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                if (i == 0 && j == 0)
+                {
+                    Instantiate(gridPlane, new Vector3(i * xSize, 0, j * ySize), Quaternion.identity);
+                }
+                else if (j == 0)
+                {
+                    Instantiate(gridPlane, new Vector3(i * xSize, 0, j * ySize), Quaternion.identity);
+                    Instantiate(gridPlane, new Vector3(i * -xSize, 0, j * ySize), Quaternion.identity);
+                }
+                else if (i == 0)
+                {
+                    Instantiate(gridPlane, new Vector3(i * xSize, 0, j * ySize), Quaternion.identity);
+                    Instantiate(gridPlane, new Vector3(i * xSize, 0, j * -ySize), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(gridPlane, new Vector3(i * xSize, 0, j * ySize), Quaternion.identity);
+                    Instantiate(gridPlane, new Vector3(i * xSize, 0, j * -ySize), Quaternion.identity);
+                    Instantiate(gridPlane, new Vector3(i * -xSize, 0, j * ySize), Quaternion.identity);
+                    Instantiate(gridPlane, new Vector3(i * -xSize, 0, j * -ySize), Quaternion.identity);
+                }
+            }
+        }
+
+    }
+}
