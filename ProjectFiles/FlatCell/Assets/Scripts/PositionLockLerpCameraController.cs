05dca4842f00ce02dd95649f6ca1a1577e951a67
David Lee <jeoungwoo97@gmail.com>
2019-06-03 19:48:04 -0700

    Pulled from Dev
    Unity game from Dev

---
+++ ProjectFiles/FlatCell/Assets/Scripts/PositionLockLerpCameraController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Obscura
+{
+    public class PositionLockLerpCameraController : AbstractCameraController
+    {
+        [SerializeField] private float LerpDuration; 
+        private Camera ManagedCamera;
+        private LineRenderer CameraLineRenderer;
+        private Vector3 previousLoc;
+        private float timeCounter = 0.0f;
+
+        private void Awake()
+        {
+            this.ManagedCamera = this.gameObject.GetComponent<Camera>();
+            this.CameraLineRenderer = this.gameObject.GetComponent<LineRenderer>();
+        }
+
+        private void OnEnable()
+        {
+            // Move camera to player position.
+            var targetPosition = this.Target.transform.position;
+            var cameraPosition = this.ManagedCamera.transform.position;
+            cameraPosition.x = targetPosition.x;
+            cameraPosition.y = targetPosition.y;
+            this.ManagedCamera.transform.position = cameraPosition;
+            previousLoc = this.Target.transform.position;
+        }
+
+        //Use the LateUpdate message to avoid setting the camera's position before
+        //GameObject locations are finalized.
+        void LateUpdate()
+        {
+            var cameraPosition = this.ManagedCamera.transform.position;
+
+            if(this.Target.transform.hasChanged)
+            {
+                previousLoc = this.Target.transform.position;
+                timeCounter = 0.0f;
+            }
+
+            timeCounter += Time.deltaTime;
+
+            if(timeCounter < LerpDuration)
+            {
+                timeCounter += Time.deltaTime;
+            }
+
+            var adjust = Vector2.Lerp(new Vector2(cameraPosition.x, cameraPosition.y), new Vector2(previousLoc.x, previousLoc.y), timeCounter/ LerpDuration);
+            cameraPosition = new Vector3(adjust.x, adjust.y, cameraPosition.z);
+
+            this.ManagedCamera.transform.position = cameraPosition;
+
+            if (this.DrawLogic)
+            {
+                this.CameraLineRenderer.enabled = true;
+                this.DrawCameraLogic();
+            }
+            else
+            {
+                this.CameraLineRenderer.enabled = false;
+            }
+        }
+
+        public override void DrawCameraLogic()
+        {
+            this.CameraLineRenderer.positionCount = 8;
+            this.CameraLineRenderer.useWorldSpace = false;
+            this.CameraLineRenderer.SetPosition(0, new Vector3(0, 0, 85));
+            this.CameraLineRenderer.SetPosition(1, new Vector3(5, 0, 85));
+            this.CameraLineRenderer.SetPosition(2, new Vector3(0, 0, 85));
+            this.CameraLineRenderer.SetPosition(3, new Vector3(-5, 0, 85));
+            this.CameraLineRenderer.SetPosition(4, new Vector3(0, 0, 85));
+            this.CameraLineRenderer.SetPosition(5, new Vector3(0, 5, 85));
+            this.CameraLineRenderer.SetPosition(6, new Vector3(0, 0, 85));
+            this.CameraLineRenderer.SetPosition(7, new Vector3(0, -5, 85));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # ECS189L-Group-Project
 Group members- Megan Brown, Brian Coe, David Lee, Kyle Catapusan, Nick Stapleton
+
+## Install Instructions ##
+These instructions will get you the most recent version of `dev`.
+### Dev ###
+#### Command line ####
+```
+git clone https://github.com/nhstaple/ECS189L-Group-Project
+cd ECS189L-Group-Project
+git branch dev
+git checkout dev
+git pull origin dev
+```
+
+#### Github.com ####
+![Img1](https://github.com/nhstaple/ECS189L-Group-Project/blob/dev/meta/ecs%20189l%20git%20tut/img1.jpg?raw=true)
+![Img2](https://github.com/nhstaple/ECS189L-Group-Project/blob/dev/meta/ecs%20189l%20git%20tut/img2.jpg?raw=true)
+![Img3](https://github.com/nhstaple/ECS189L-Group-Project/blob/dev/meta/ecs%20189l%20git%20tut/img3.jpg?raw=true)
+![Img4](https://github.com/nhstaple/ECS189L-Group-Project/blob/dev/meta/ecs%20189l%20git%20tut/img4.jpg?raw=true)
+
+## About ##
+Project Description.
+
+## Bios ##
+Write our bios so the press kit homie can grab it from here. 
+
+### Megan ###
+
+### Brian ###
+
+### David ###
+
+### Kyle ###
+
+### Nick ###
+Roles- Game Logic, Narrative Design.
+
+Assisted with Animation/Visuals and Game Feel on flat game?
+
+* 5th year
+* Major: Computer Science
+* Minor: Moosic 🐮
+
+## Roles ##
+[Link to project details.](https://github.com/dr-jam/ECS189L/blob/master/GameProject.md)
+
+Please add the roles that you're interested in.
+
+| Role | Name |
+| ---- | ---- |
+| UI | David |
+| Movement/Physics | Brian |
+| Animation/Visuals | Megan |
+| Input | Kyle |
+| Game Logic | Nick |
+
+| Subrole | Name | 
+| ------- | ---- |
+| Audio | Megan |
+| Gameplay Testing | Kyle | 
+| Narrative Design | Nick |
+| Press Kit and Trailer | Brian | 
+| Game Feel | David |
+
+```
+Environment (parallax),  David
+```
