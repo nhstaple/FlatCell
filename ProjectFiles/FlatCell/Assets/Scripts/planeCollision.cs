11d10c3c02744178ebb538a5d04cc98155bf7a4f
Brian <briancoe@gmail.com>
2019-06-06 09:44:56 -0700

    Update planeCollision.cs
    changed to ontrigger

--- ProjectFiles/FlatCell/Assets/Scripts/planeCollision.cs
+++ ProjectFiles/FlatCell/Assets/Scripts/planeCollision.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class planeCollision : MonoBehaviour
 {
 
-
-
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {
         GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
     }
