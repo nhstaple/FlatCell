1e0a562423ab450f53ee3632cfd4356da0a8bb04
nhstaple <nicholas.stapleton97@gmail.com>
2019-06-28 16:00:03 -0700

    startmenu rework

--- ProjectFiles/FlatCell/Assets/Scripts/UI/StartMenu.cs
+++ ProjectFiles/FlatCell/Assets/Scripts/UI/StartMenu.cs
@@ -2,18 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class StartMenu : MonoBehaviour
 {
-    private string Start = "Game";
+    private string start = "Game";
     private string Controls = "Control";
     private string About = "Abeut";
 
+    [SerializeField] TextMeshProUGUI title;
+    [SerializeField] TextMeshProUGUI[] menu;
+
+    [SerializeField] float fadeInLerpTime = 3f;
+    [SerializeField] float menuDelay = 1f;
+    float fadeInCounter;
+
+
+    void Start()
+    {
+        title.fontSharedMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, -1f);
+        foreach(TextMeshProUGUI sub in menu)
+        {
+            sub.fontSharedMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, -1f);
+        }
+        fadeInCounter = 0f;
+    }
+
+    private void Update()
+    {
+        float alpha = Mathf.SmoothStep(-1f, -.25f, fadeInCounter / fadeInLerpTime);
+        float beta = Mathf.SmoothStep(-.8f, 0, (fadeInCounter - menuDelay) / fadeInLerpTime);
+
+        title.fontSharedMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, alpha);
+
+        foreach (TextMeshProUGUI sub in menu)
+        {
+            sub.fontSharedMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, beta);
+        }
+
+        fadeInCounter += Time.deltaTime;
+    }
+
     public GameObject StartScreen;
     // Update is called once per frame
-    public void StarMenu()
+    public void StartGame()
     {
-        SceneManager.LoadScene(Start);
+        SceneManager.LoadScene(start);
     }
     public void Control()
     {
