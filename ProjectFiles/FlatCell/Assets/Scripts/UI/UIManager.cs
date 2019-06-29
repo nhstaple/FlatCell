eb03a1599e3892ea1201f2deda0f231a82ffc53e
nhstaple <nicholas.stapleton97@gmail.com>
2019-06-28 18:57:36 -0700

    HUD fades in

--- ProjectFiles/FlatCell/Assets/Scripts/UI/UIManager.cs
+++ ProjectFiles/FlatCell/Assets/Scripts/UI/UIManager.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Controller.Player;
+using TMPro;
+using UnityEngine.UI;
 
 namespace Utils.UIManager
 {
@@ -12,6 +14,16 @@ namespace Utils.UIManager
     {
         GameObject player;
         public PlayerController controller;
+
+        [SerializeField] Image[] panels;
+        [SerializeField] Image[] meterBars;
+        [SerializeField] Image[] meterBackgrounds;
+        [SerializeField] TextMeshProUGUI[] textBoxTitles;
+        [SerializeField] TextMeshProUGUI[] textBoxInfo;
+        [SerializeField] TextMeshProUGUI[] textBoxBackground;
+
+        [SerializeField] float panelFadeIn = 1f;
+        float fadeInCounter = 0f;
 
         public GameObject GetPlayerObject()
         {
@@ -47,12 +59,68 @@ namespace Utils.UIManager
         void Start()
         {
             GetPlayer();
+            foreach(Image panel in panels)
+            {
+                panel.color = new Color(0,0,0,0);
+            }
+            foreach (Image bar in meterBars)
+            {
+                bar.color = new Color(bar.color.r, bar.color.g, bar.color.b, 0);
+            }
+            foreach(Image back in meterBackgrounds)
+            {
+                back.color = new Color(back.color.r, back.color.g, back.color.b, 0);
+            }
+
+            foreach (TextMeshProUGUI panel in textBoxInfo)
+            {
+                panel.color = new Color(1, 1, 1, 0);
+            }
+
+            foreach (TextMeshProUGUI text in textBoxTitles)
+            {
+                text.color = new Color(1, 1, 1, 0);
+            }
+            foreach (TextMeshProUGUI text in textBoxBackground)
+            {
+                text.color = new Color(1, 1, 1, 0);
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
             GetPlayer();
+            float alpha = Mathf.SmoothStep(0, 240f / 255, fadeInCounter / panelFadeIn);
+            float beta = Mathf.SmoothStep(0, 128f / 255, fadeInCounter / panelFadeIn);
+            float gamma = Mathf.SmoothStep(0, 64f / 255, fadeInCounter / panelFadeIn);
+            float boop = Mathf.SmoothStep(0, 16f / 255, fadeInCounter / panelFadeIn);
+
+            foreach (Image panel in panels)
+            {
+                panel.color = new Color(0, 0, 0, alpha);
+            }
+            foreach (TextMeshProUGUI text in textBoxTitles)
+            {
+                text.color = new Color(1, 1, 1, alpha);
+            }
+            foreach (Image bar in meterBars)
+            {
+                bar.color = new Color(bar.color.r, bar.color.g, bar.color.b, alpha);
+            }
+            foreach (Image back in meterBackgrounds)
+            {
+                back.color = new Color(back.color.r, back.color.g, back.color.b, gamma);
+            }
+            foreach (TextMeshProUGUI panel in textBoxInfo)
+            {
+                panel.color = new Color(1, 1, 1, beta);
+            }
+            foreach (TextMeshProUGUI text in textBoxBackground)
+            {
+                text.color = new Color(1, 1, 1, boop);
+            }
+            fadeInCounter += Time.deltaTime;
         }
     }
 }
